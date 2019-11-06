eedcd6d23ace677e5be8296cb1850211550261b8
jhowarth <jahowarth@gmail.com>
2019-11-06 13:11:37 -0800

    update to c++ and fix openssl macos issue Signed-off-by: Jesse Howarth jahowarth@gmail.com

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("cargo:rustc-flags=-l dylib=cassandra");
     println!("cargo:rustc-flags=-l dylib=crypto");
     println!("cargo:rustc-flags=-l dylib=ssl");
-    println!("cargo:rustc-flags=-l dylib=stdc++");
+    println!("cargo:rustc-flags=-l dylib=c++");
     println!("cargo:rustc-flags=-l dylib=uv");
     println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
@@ -16,4 +16,6 @@ fn main() {
     println!("cargo:rustc-link-search={}", "/usr/local/lib");
     println!("cargo:rustc-link-search={}", "/usr/lib64/");
     println!("cargo:rustc-link-search={}", "/usr/lib/");
+    println!("cargo:rustc-link-search={}", "/usr/local/opt/openssl/lib");
+    
 }
